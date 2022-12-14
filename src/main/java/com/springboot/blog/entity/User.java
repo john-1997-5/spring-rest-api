9374b1cb6c5320cfdae309ec262d807310cd8a33
Johnson <johnson.arrobo@gmail.com>
2022-12-14 17:40:03 +0100

    88. creadas 2 entidades nuevas con relación ManyToMany (e.b)

---
+++ src/main/java/com/springboot/blog/entity/User.java
@@ -0,0 +1,27 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = {"username", "email"})})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", // nombre de la tabla temporal que tiene las PK de ambas entidades
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), // referencia al id de User
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")) // referencia al id de Role
+    private Set<Role> roles;
+}
