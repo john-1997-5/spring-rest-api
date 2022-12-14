425a33c2718434534d4e115f8cfb820c059a7065
Johnson <johnson.arrobo@gmail.com>
2022-12-14 13:35:11 +0100

    FIX: Post entity with no comments (e.b)

--- src/main/java/com/springboot/blog/entity/Comment.java
+++ src/main/java/com/springboot/blog/entity/Comment.java
@@ -1,11 +1,13 @@
 package com.springboot.blog.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "comments")
-@Data
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
